4ab79c1fb151779105a57107db2dd166a8079195
Serpil-Dndr <serpil.dndrca@gmail.com>
2023-05-20 00:05:37 -0400

    added some codingbat answer

---
+++ src/codingBat_Practice/Makes_10.java
@@ -0,0 +1,30 @@
+package codingBat_Practice;
+
+public class Makes_10 {
+    public static void main(String[] args) {
+//        Given 2 ints, a and b, return true if one of them is 10 or if their sum is 10.
+//
+//        makes10(9, 10) → true
+//        makes10(9, 9) → false
+//        makes10(1, 9) → true
+        System.out.println(makes10(1, 9));
+        System.out.println(makes10(9, 9));
+        System.out.println(makes10(9, 10));
+    }
+    public static boolean makes10(int a, int b) {
+        //1.yol
+//         if (a== 10 || b==10){
+//             return true;
+//         }
+//         if( a+b==10){
+//             return true;
+//         }
+//         return false;
+//    }
+        //2.yol
+                return (a== 10 || b==10 || a+b==10);
+
+        }
+
+
+}
---
+++ src/codingBat_Practice/NotString.java
@@ -0,0 +1,34 @@
+package codingBat_Practice;
+
+public class NotString {
+    public static void main(String[] args) {
+        /*
+        Given a string, return a new string where "not " has been added to the front. However,
+        if the string already begins with "not", return the string unchanged.
+        Note: use .equals() to compare 2 strings.
+notString("candy") → "not candy"
+notString("x") → "not x"
+notString("not bad") → "not bad"
+         */
+        System.out.println(notString("candy"));
+        System.out.println(notString("x"));
+        System.out.println(notString("not bad"));
+    }
+    public static String notString(String str) {
+
+//        if(!str.startsWith("not")){
+//            return "not "+ str;
+//        }
+//
+//           return str;
+
+   // }
+        if (str.length() >= 3 && str.substring(0, 3).equals("not"))
+         {
+            return str;
+
+        }
+        return "not "+str;
+        }
+
+}
